a44ed6ef85629fb337caa5a050077914ed8871a8
Suleman <sulemanhamza97@gmail.com>
2023-01-08 21:54:54 +0500

    Initial Commit - Empty screen with paddles configured

---
+++ main.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gdamore/tcell"
+	"github.com/gdamore/tcell/encoding"
+	"os"
+)
+
+const PaddleHeight = 4
+const PaddleSymbol = 0x2588
+
+type Paddle struct {
+	row, col, width, height int
+}
+
+var screen tcell.Screen
+var player1 *Paddle
+var player2 *Paddle
+
+func PrintString(col, row int, str string) {
+	for _, c := range str {
+		screen.SetContent(col, row, c, nil, tcell.StyleDefault)
+		col += 1
+	}
+}
+
+func Print(col, row, width, height int, ch rune) {
+	for r := 0; r < height; r++ {
+		for c := 0; c < width; c++ {
+			screen.SetContent(col+c, row+r, ch, nil, tcell.StyleDefault)
+		}
+	}
+}
+
+func DrawState() {
+	screen.Clear()
+	Print(player1.col, player1.row, player1.width, player1.height, PaddleSymbol)
+	Print(player2.col, player2.row, player2.width, player2.height, PaddleSymbol)
+	//PrintString(screen, 0, 0, "Hello, World!")
+	screen.Show()
+}
+
+// This program just prints "Hello, World!".  Press ESC to exit.
+func main() {
+	InitScreen()
+	InitGameState()
+	DrawState()
+
+	for {
+		switch ev := screen.PollEvent().(type) {
+		case *tcell.EventKey:
+			if ev.Key() == tcell.KeyEscape {
+				screen.Fini()
+				os.Exit(0)
+			}
+		}
+	}
+}
+
+func InitScreen() {
+	encoding.Register()
+	var err error
+	screen, err = tcell.NewScreen()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", err)
+		os.Exit(1)
+	}
+	if e := screen.Init(); e != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", e)
+		os.Exit(1)
+	}
+
+	defStyle := tcell.StyleDefault.
+		Background(tcell.ColorBlack).
+		Foreground(tcell.ColorWhite)
+	screen.SetStyle(defStyle)
+}
+
+func InitGameState() {
+	width, height := screen.Size()
+	paddleStart := height/2 - PaddleHeight/2
+
+	player1 = &Paddle{
+		row: paddleStart, col: 0, width: 1, height: PaddleHeight,
+	}
+
+	player2 = &Paddle{
+		row: paddleStart, col: width - 1, width: 1, height: PaddleHeight,
+	}
+}
